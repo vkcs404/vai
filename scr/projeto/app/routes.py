97c32fc9055b0758ccef8b22236f3b9d5fb254e3
Yasmin Lima <yasmin.geovana@pucpr.edu.br>
2025-09-29 14:55:47 -0300

    criei as rotas de cadastro,login,pagamento,scaner e relatorios

--- scr/projeto/app/routes.py
+++ scr/projeto/app/routes.py
@@ -6,3 +6,28 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+# Rota para cadastrar
+@main_bp.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+# Rota para logar
+@main_bp.route('/login')
+def login():
+    return render_template('login.html')
+
+# Rota para pagamente
+@main_bp.route('/pagamento')
+def pagamento():
+    return render_template('pagamento.html')
+
+# Rota para scanear
+@main_bp.route('/scaner')
+def scaner():
+    return render_template('scaner.html')
+
+# Rota para ver os relatorios 
+@main_bp.route('/relatorio')
+def relatorio():
+    return render_template('relatorio.html')
--- scr/projeto/main.py
+++ scr/projeto/main.py
@@ -2,6 +2,6 @@ from app import create_app
 
 app = create_app()
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
 
